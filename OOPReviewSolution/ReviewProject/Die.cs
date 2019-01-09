a9b091d0546246c413fca55fca678261b0473ae1
jabdala1 <46543609+jabdala1@users.noreply.github.com>
2019-01-09 16:48:07 -0700

    Create Die Class

---
+++ OOPReviewSolution/ReviewProject/Die.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewProject
+{
+    public class Die
+    {
+        //this is the definition of a object
+        //it is a conceptual view of the data
+        //that will be held by a physical 
+        //instance (object) of this class
+
+        //Components of an object
+
+        //Date Members
+        //is an internal private date storage item
+        //private data memebers CANNOT be reached directly by the user
+        //public data memebers CAN be reached directly by the user
+        private int _Size;
+        private string _Color;
+        private int _Face;
+
+        //Properties
+        //A property is an external interface between the user and a single piece of data within the instance
+        //A property has two ability
+        // a) the ability to assign a value to the internal data member
+        // b) return a internal data memeber value to the user
+
+        //Fully Implemented Property
+        public int Size
+        {
+            get
+            {
+                //takes internal values and returns it to the user
+                return _Size;
+            }
+            set
+            {
+                //takes the supplied user value and place it into the internal private data member
+                //the incoming piece of data is place into a special variable that is called: value
+                _Size = value;
+            }
+        }
+        //Constructors
+
+        //Behaviours (methods)
+    }
+}
---
+++ OOPReviewSolution/ReviewProject/OOPsReview.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewProject
+{
+    class OOPsReview
+    {
+        static void Main(string[] args)
+        {
+            //Date Member
+            //Properties - the interface of the instance for a a data member to a user
+            //Constructor - use to setup an initial/default state of your obeject (instance)
+            //Methods - actions available to the user
+            //Example of a method. Roll() - randomly create a value between a min and a max, assigned to the face property
+            //describe
+            //define
+            //declare
+            //specify
+            //Programmer defined datatype
+            //Developer defined datetype
+
+            //Steps to code
+            //Understand the problem - make a die
+            //Design Solution - make the design of the dice
+            //Code
+        }
+    }
+}
