447e0b4d478b731fef3c9ab968afdf90289ac302
jabdala1 <46543609+jabdala1@users.noreply.github.com>
2019-01-11 16:13:09 -0700

    Update Die

--- OOPReviewSolution/ReviewProject/Die.cs
+++ OOPReviewSolution/ReviewProject/Die.cs
@@ -21,7 +21,7 @@ namespace ReviewProject
         //public data memebers CAN be reached directly by the user
         private int _Size;
         private string _Color;
-        private int _Face;
+        //private int _FaceValue;
 
         //Properties
         //A property is an external interface between the user and a single piece of data within the instance
@@ -41,9 +41,44 @@ namespace ReviewProject
             {
                 //takes the supplied user value and place it into the internal private data member
                 //the incoming piece of data is place into a special variable that is called: value
-                _Size = value;
+                //optionally you may place validation on the incoming value
+                if (value >= 6 && value <= 20)
+                {
+                    _Size = value;
+                }
+                else
+                {
+                    throw new Exception("Die cannot be " + value.ToString() + " sides. Die must have between 6 and 20 sides.");
+                }
             }
         }
+        //Auto Implemented Property
+        //Public, it has a datatype, it has a name, IT does not have an internal data member that you can DIRECTLY access.
+        //The system will create, internally, a data storage area of the appropriate datatype and manage the area.
+        //The only way to access the data of an Auto Implemented Property is via the property, usually used when their is no need for any internal validation or other property logics.
+        public int FaceValue { get; set; }
+
+        public string Color
+        {
+            get
+            {
+                return _Color;
+            }
+            set
+            {
+                // (value == null) this will fail for an empty string
+                // (value == "") this will fail for a null value
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("The color field cannot be blank");
+                }
+                else
+                {
+                    _Color = value;
+                }
+            }
+        }
+
         //Constructors
 
         //Behaviours (methods)
