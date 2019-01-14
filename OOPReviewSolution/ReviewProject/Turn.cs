ffa46303e299ab1678a530c065c702cb55c90f3a
jabdala1 <46543609+jabdala1@users.noreply.github.com>
2019-01-14 16:54:12 -0700

    Update Turn.cs

--- OOPReviewSolution/ReviewProject/Turn.cs
+++ OOPReviewSolution/ReviewProject/Turn.cs
@@ -6,8 +6,39 @@ using System.Threading.Tasks;
 
 namespace ReviewProject
 {
-    class Turn
+    public class Turn
     {
+        private int _playerOneRoll;
         
+        public int PlayerOneRoll
+        {
+            get
+            {
+                return _playerOneRoll;
+            }
+            set
+            {
+                _playerOneRoll = value;
+            }
+        }
+
+        public int PlayerTwoRoll { get; set; }
+        
+        public Turn()
+        {
+
+        }
+
+        public Turn(int playerTwo, int playerOne)
+        {
+            PlayerOneRoll = playerOne;
+            PlayerTwoRoll = playerTwo;
+        }
+
+        //methods
+        //public string FindRollResults()
+        //{
+        //  return winner;
+        //}
     }
 }
