fd3aac812cf024fbfe9c360f13b1a1af0395c307
jabdala1 <46543609+jabdala1@users.noreply.github.com>
2019-01-14 15:26:40 -0700

    Update Die.cs

--- OOPReviewSolution/ReviewProject/Die.cs
+++ OOPReviewSolution/ReviewProject/Die.cs
@@ -19,7 +19,7 @@ namespace ReviewProject
         //is an internal private date storage item
         //private data memebers CANNOT be reached directly by the user
         //public data memebers CAN be reached directly by the user
-        private int _Size;
+        private int _Side;
         private string _Color;
         private int _Face;
 
@@ -30,21 +30,37 @@ namespace ReviewProject
         // b) return a internal data memeber value to the user
 
         //Fully Implemented Property
-        public int Size
+        public int Side
         {
             get
             {
                 //takes internal values and returns it to the user
-                return _Size;
+                return _Side;
             }
             set
             {
                 //takes the supplied user value and place it into the internal private data member
                 //the incoming piece of data is place into a special variable that is called: value
-                _Size = value;
+                _Side = value;
             }
         }
+
         //Constructors
+
+        //Optional: if not supplied the system default constructor is used which will assign a value to each data member/auto implemented property according to its data type.
+        //You can have any number of contructors within your class as soon as you code a contructor, your program is responsible for all constructors
+
+        //sytanx of a constructor: public classmate([list of prameters]) {...}
+
+        //Typical Constructors
+        //Default Constructors
+        //This is similar to the system default contructor
+        public Die()
+        {
+            //You can directly access a private data member in any place within your class
+            _Side = 6;
+            _Color = "";
+        }
 
         //Behaviours (methods)
     }
